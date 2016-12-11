c8e4d30e36e62677704cd453b2b7d2812439be4c
Brent Perry <burentoperii@gmail.com>
2016-12-11 16:57:52 -0600

    complete all functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,76 @@ $("document").ready(function () {
   $(".javascript").click(function () {
     $(".javascript").toggle();
   });
+
+  $(".operator").click(function () {
+    $(".operator").toggle();
+  });
+
+  $(".variable").click(function () {
+    $(".variable").toggle();
+  });
+
+  $(".naming").click(function () {
+    $(".naming").toggle();
+  });
+
+  $(".function").click(function () {
+    $(".function").toggle();
+  });
+
+  $(".method").click(function () {
+    $(".method").toggle();
+  });
+
+  $(".argument").click(function () {
+    $(".argument").toggle();
+  });
+
+  $(".parameter").click(function () {
+    $(".parameter").toggle();
+  });
+
+  $(".return").click(function () {
+    $(".return").toggle();
+  });
+
+  $(".chain").click(function () {
+    $(".chain").toggle();
+  });
+
+  $(".string").click(function () {
+    $(".string").toggle();
+  });
+
+  $(".boolean").click(function () {
+    $(".boolean").toggle();
+  });
+
+  $(".undefined").click(function () {
+    $(".undefined").toggle();
+  });
+
+  $(".nan").click(function () {
+    $(".nan").toggle();
+  });
+
+  $(".escape").click(function () {
+    $(".escape").toggle();
+  });
+
+  $(".alert").click(function () {
+    $(".alert").toggle();
+  });
+
+  $(".comment").click(function () {
+    $(".comment").toggle();
+  });
+
+  $(".jquery").click(function () {
+    $(".jquery").toggle();
+  });
+
+  $(".attribute").click(function () {
+    $(".attribute").toggle();
+  });
 });
